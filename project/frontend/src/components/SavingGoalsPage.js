eeda278fe9399f113f97c556c02b1ffcb50e9dca
Brandon <hoangbrandon@outlook.com>
2025-04-05 23:42:34 -0700

    test? saving goals? idk

---
+++ project/frontend/src/components/SavingGoalsPage.js
@@ -0,0 +1,13 @@
+// frontend/src/components/SavingGoalsPage.js
+import React from 'react';
+
+function SavingGoalsPage() { // Changed component name
+  return (
+    <div>
+      <h1>Saving Goals Page</h1> {/* Updated heading */}
+      {/* Add your saving goals content here */}
+    </div>
+  );
+}
+
+export default SavingGoalsPage; // Changed export name
